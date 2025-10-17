8933b6db86e4dab57764d16835cf055a87b234f1
miguel silva <miguelfbs02@gmail.com>
2025-10-16 21:01:35 -0300

    classe position criada

--- src/application/program.java
+++ src/application/program.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class program {
 
 	public static void main(String[] args) {
-		System.out.println("ola mundo!");
+		Position p=new Position(3, 5);
+		System.out.println(p);
 
 	}
 
